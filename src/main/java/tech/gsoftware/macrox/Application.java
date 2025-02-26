78d4a9f5c72d947fcf6f82c89a5fdb42a39d0b80
woz07 <muhsameer9@gmail.com>
2025-02-26 01:21:47 +0000

    add: logger, https://github.com/woz07/lwlfj

--- src/main/java/tech/gsoftware/macrox/Application.java
+++ src/main/java/tech/gsoftware/macrox/Application.java
@@ -1,8 +1,11 @@
 package tech.gsoftware.macrox;
 
+import dev.woz07.lwlfj.Logger;
+
 import javax.swing.*;
 
 public class Application extends JFrame {
+    public Logger logger = new Logger();
     public Application() {
 
     }
