56652f961daa3d1ed1c0bad8669a62a5e99529fd
woz07 <muhsameer9@gmail.com>
2025-02-26 01:27:57 +0000

    add: Json.java, which reads from json and writes to json

---
+++ src/main/java/tech/gsoftware/macrox/data/Json.java
@@ -0,0 +1,41 @@
+package tech.gsoftware.macrox.data;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.woz07.lwlfj.Logger;
+import dev.woz07.lwlfj.color.Background;
+import dev.woz07.lwlfj.color.Foreground;
+import dev.woz07.lwlfj.level.Severity;
+import tech.gsoftware.macrox.Application;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Json {
+    private final Logger logger;
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    public Json(Logger logger) {
+        this.logger = logger;
+        this.logger.setBackground(Background.WHITE);
+        this.logger.setForeground(Foreground.RED);
+    }
+
+    public Data read(String path) {
+        try {
+            return mapper.readValue(new File(path), Data.class);
+        } catch (IOException e) {
+            logger.log(Json.class, Severity.SEVERE, "Unable to read data from path: '" + path + "'.");
+            return null;
+        }
+    }
+
+    public boolean write(Data data, String path) {
+        try {
+            mapper.writeValue(new File(path), data);
+            return true;
+        } catch (IOException e) {
+            logger.log(Json.class, Severity.SEVERE, "Unable to write data to path: '" + path + "'.");
+            return false;
+        }
+    }
+}
